432587f7502b4f2cb7e0c442ec310225b88c29cc
Nick Parsons <nick.parsons@warwick.ac.uk>
2013-11-11 00:00:00 +0000

    version 2.0

--- R/combn.test.R
+++ R/combn.test.R
@@ -1,56 +1,37 @@
 combn.test <-
-function(stage1,stage2,weight=0.5,method="invnorm"){
-
- # valid test type
- meths <- c("invnorm", "fisher")
- imeths <- as.integer(match(method, meths, -1))
- if (imeths < 1) {
-  stop("Invalid combination test type")
- } # end if
-
- # extract pvalues from dunnett test object
- p1 <- stage1$pvalues
- p2 <- stage2$pvalues
-
- # intersection hypotheses indicator
- hyp.comb <- stage1$hyp.comb
-
- # number of treatments is length of Z vector
- treats <- length(p1)
-
- # weights
- if (method=="invnorm"){ 
-  w1 <- sqrt(weight)
-  w2 <- sqrt(1-w1*w1)
- } else {
-  w1 <- 1; w2 <- 1
- } # end if
-
- # structure zscores same as p1
- zscores <- p1
- 
- # identify elements of zscores
- for (i in 1:treats){
-  for (j in 1:length(p1[[i]])){
-  
-   # replace with missing value indicator
-   zscores[[i]][j] <- NA
-   
-   # inverse normal combination
-   if (method=="invnorm"){
-    zscores[[i]][j] <- w1*qnorm(1-p1[[i]][j])+w2*qnorm(1-p2[[i]][j])
-   } # end if
-
-   # fisher combination
-   if (method=="fisher"){
-    zscores[[i]][j] <- qnorm(pchisq(-2*log(p1[[i]][j]*p2[[i]][j]),4))
-   } # end if
-
-  } # end j
- } # end i
-
- # results of combination test
- list(method=method,zscores=zscores,hyp.comb=hyp.comb,weights=c(w1,w2))
-
-} # end of function combn.test
-
+function (stage1, stage2, weight = 0.5, method = "invnorm") 
+{
+    meths <- c("invnorm", "fisher")
+    imeths <- as.integer(match(method, meths, -1))
+    if (imeths < 1) {
+        stop("Invalid combination test type")
+    }
+    p1 <- stage1$pvalues
+    p2 <- stage2$pvalues
+    hyp.comb <- stage1$hyp.comb
+    treats <- length(p1)
+    if (method == "invnorm") {
+        w1 <- sqrt(weight)
+        w2 <- sqrt(1 - w1 * w1)
+    }
+    else {
+        w1 <- 1
+        w2 <- 1
+    }
+    zscores <- p1
+    for (i in 1:treats) {
+        for (j in 1:length(p1[[i]])) {
+            zscores[[i]][j] <- NA
+            if (method == "invnorm") {
+                zscores[[i]][j] <- w1 * qnorm(1 - p1[[i]][j]) + 
+                  w2 * qnorm(1 - p2[[i]][j])
+            }
+            if (method == "fisher") {
+                zscores[[i]][j] <- qnorm(pchisq(-2 * log(p1[[i]][j] * 
+                  p2[[i]][j]), 4))
+            }
+        }
+    }
+    list(method = method, zscores = zscores, hyp.comb = hyp.comb, 
+        weights = c(w1, w2))
+}
--- R/dunnett.test.R
+++ R/dunnett.test.R
@@ -1,75 +1,51 @@
 dunnett.test <-
-function(Z=Z,select=rep(1,length(Z))){
-
- # number of treatments is length of Z vector
- treats <- length(Z)
-
- # intersection hypotheses indicator
- hyp.comb <- list(NULL); nhyp.comb <- vector(length=treats)
- for (i in 1:treats){
-  comb.dat <- combn(1:treats,i)
-  hyp.comb[[i]] <- comb.dat
-  nhyp.comb[i] <- ncol(hyp.comb[[i]])
-  rownames(hyp.comb[[i]]) <- 1:i
-  hypcol <- NULL
-  for (j in 1:nhyp.comb[i]){
-   ihypcol <- paste("H",paste(hyp.comb[[i]][,j],collapse=""),sep="")
-   hypcol <- append(hypcol,ihypcol)
-  } # end j
-  colnames(hyp.comb[[i]]) <- hypcol
- } # end i
-
- # test results
- pdunnett.test <- list(NULL)
- zscores <- list(NULL)
-
- # dunnett function
- int_dunnett <- function(x,z,k){
-  ((pnorm(sqrt(2)*z+x))^k)*dnorm(x)
- }
-
- # evaluate integral 
- for (i in 1:treats){
-
-  # i = number of elements in intersection hypothesis
-  ptest <- NULL
-
-  # set Z to -Inf if not selected
-  if(select[i]==0){
-   Z[i] <- -Inf
-  } # end if
-
-  for (j in 1:nhyp.comb[i]){
-
-   # calculate correct k
-   kselect <- sum(select[c(hyp.comb[[i]][,j])])
-
-   # select maximum 
-   Zmax <- max(Z[c(hyp.comb[[i]][,j])])
-
-   # calculate integral for z=Zmax and k=kselect
-   if (kselect==0){
-    dunnet_integral <- 0
-    F_Zmax <- 1
-   } else {
-    dunnett_integral <- integrate(int_dunnett,-Inf,Inf,z=Zmax,k=kselect)
-    F_Zmax <- 1-dunnett_integral$value
-   } # end if
-
-   ptest <- append(ptest,F_Zmax)
-   
-  } # end j
-
-  # store results
-  pdunnett.test[[i]] <- matrix(ptest,nrow=1,ncol=length(ptest))
-  colnames(pdunnett.test[[i]]) <- colnames(hyp.comb[[i]])
-  rownames(pdunnett.test[[i]]) <- 1
-  zscores[[i]] <- qnorm(1-pdunnett.test[[i]])
-
- } # end i
-
- # results of Dunnett's tests
- list(pvalues=pdunnett.test,zscores=zscores,hyp.comb=hyp.comb)
-
-} # end of function dunnett.test
-
+function (Z = Z, select = rep(1, length(Z))) 
+{
+    if(sum(is.na(Z))>0){stop("No missing values allowed in Z")}
+    treats <- length(Z)
+    hyp.comb <- list(NULL)
+    nhyp.comb <- vector(length = treats)
+    for (i in 1:treats) {
+        comb.dat <- combn(1:treats, i)
+        hyp.comb[[i]] <- comb.dat
+        nhyp.comb[i] <- ncol(hyp.comb[[i]])
+        rownames(hyp.comb[[i]]) <- 1:i
+        hypcol <- NULL
+        for (j in 1:nhyp.comb[i]) {
+            ihypcol <- paste("H", paste(hyp.comb[[i]][, j], collapse = ""), 
+                sep = "")
+            hypcol <- append(hypcol, ihypcol)
+        }
+        colnames(hyp.comb[[i]]) <- hypcol
+    }
+    pdunnett.test <- list(NULL)
+    zscores <- list(NULL)
+    int_dunnett <- function(x, z, k) {
+        ((pnorm(sqrt(2) * z + x))^k) * dnorm(x)
+    }
+    for (i in 1:treats) {
+        ptest <- NULL
+        if (select[i] == 0) {
+            Z[i] <- -Inf
+        }
+        for (j in 1:nhyp.comb[i]) {
+            kselect <- sum(select[c(hyp.comb[[i]][, j])])
+            Zmax <- max(Z[c(hyp.comb[[i]][, j])])
+            if (kselect == 0) {
+                dunnet_integral <- 0
+                F_Zmax <- 1
+            }
+            else {
+                dunnett_integral <- integrate(int_dunnett, -Inf, 
+                  Inf, z = Zmax, k = kselect)
+                F_Zmax <- 1 - dunnett_integral$value
+            }
+            ptest <- append(ptest, F_Zmax)
+        }
+        pdunnett.test[[i]] <- matrix(ptest, nrow = 1, ncol = length(ptest))
+        colnames(pdunnett.test[[i]]) <- colnames(hyp.comb[[i]])
+        rownames(pdunnett.test[[i]]) <- 1
+        zscores[[i]] <- qnorm(1 - pdunnett.test[[i]])
+    }
+    list(pvalues = pdunnett.test, zscores = zscores, hyp.comb = hyp.comb)
+}
